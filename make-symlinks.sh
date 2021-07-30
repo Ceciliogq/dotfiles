f133b9539195d8b5961ae0d7b96b32d6049b7453
Cecilio García-Quirós <ceciliogarciaquiros@gmail.com>
2021-07-30 17:37:25 +0200

    Backup symbolic link and regular files before overwrite. Add input arguments option

--- make-symlinks.sh
+++ make-symlinks.sh
@@ -1,16 +1,27 @@
 old_dotfiles=$HOME/.old_dotfiles
 new_dotfiles=$HOME/git/dotfiles
 
-files="vimrc bash_aliases"
 
-echo "Copying new dot files from $new_dotfiles"
+files="vimrc bash_aliases bashrc"
+
+if [ $# -eq 0 ]; then
+    echo -e "No files supplied, using default list: $files \n"
+else
+    files="$@"
+fi
+
+echo -e "Taking new dot files from $new_dotfiles \n"
 for file in $files;
 do
-    if [ -f ~/.$file ]; then
+    if [ -L $HOME/.$file ] || [ -f $HOME/.$file ] ; then
         echo "Existing .$file detected, backup to $old_dotfiles..."
-        mv ~/.$file $old_dotfiles
+        if ! [ -d $old_dotfiles ]; then
+            mkdir $old_dotfiles
+        fi
+        mv $HOME/.$file $old_dotfiles
     fi
-    echo "Creating symlink to $new_dotfiles/$file in home directory"
+    echo "Creating symlink to $file in home directory"
     ln -s $new_dotfiles/.$file $HOME/.$file
+    echo ""
 done
 echo "Done"
